c45abe0e7e03ae7c3ff41b31f3a960c508dc382d
dipdastoko <dipdastoko@gmail.com>
2021-09-27 19:47:51 +0600

    slkjf

--- js-string-array/array-slice.js
+++ js-string-array/array-slice.js
@@ -1,14 +1,14 @@
 const numbers = [3, 6, 4, 8, 9, 19, 15, 21, 45, 87];
-// const numberSliced = numbers.slice(4, 8);
-// console.log(numberSliced);
-// console.log(numbers);
+const numberSliced = numbers.slice(4, 8);
+console.log(numberSliced);
+console.log(numbers);
 
 
-// const numberSpliced = numbers.splice(4, 2);
-// console.log(numberSpliced);
-// console.log(numbers);
+const numberSpliced = numbers.splice(4, 2);
+console.log(numberSpliced);
+console.log(numbers);
 
 
 const numberSpliced2 = numbers.splice(0, 3, 1, 2, 3);
-console.log(numberSpliced2);
+console.log(numberSpliced);
 console.log(numbers);
---
+++ js-string-array/array-sort.js
@@ -0,0 +1,19 @@
+// const numbers = [4, 7, 1, 3, 9, 2, 5];
+// const sortedNumbers = numbers.sort();
+// console.log(sortedNumbers);
+
+// const friends = ['jakaria', 'karim', 'rahim', 'sham', 'abul'];
+// const sortedFriends = friends.sort();
+// console.log(sortedFriends);
+
+// const reversedFriends = friends.reverse();
+// console.log(reversedFriends);
+
+// const sortedReversedFriends = friends.sort().reverse();
+// console.log(sortedReversedFriends);
+
+const bigNumbers = [66, 58, 81, 92, 9, 6, 37, 13, 2];
+const sortedBigNumbers = bigNumbers.sort(function (a, b) {
+    return a - b;
+});
+console.log(sortedBigNumbers);
