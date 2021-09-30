105c3dbebc47c96f85710534bdd33159407bd4e0
dipdastoko <dipdastoko@gmail.com>
2021-09-30 17:20:59 +0600

    basic java

---
+++ es6-intro/basic.js
@@ -0,0 +1,45 @@
+var deposit = 400;
+//condition
+if (deposit > 0) {
+
+}
+if (deposit < 200) {
+
+}
+if (deposit == 500) {
+
+}
+if (deposit != 0) {
+
+}
+else if (deposit >= 500) {
+
+}
+else {
+
+}
+//array
+const numbers = [45, 65, 87, 124, 45, 1, 365];
+const numberCount = numbers.length;
+numbers.push(111);
+numbers.pop();
+numbers[2] = 155;
+if (numbers.indexOf(222) != -1) {
+
+}
+if (numbers.includes(222)) {
+
+}
+
+//loop
+//while, for
+for (const number of numbers) {
+
+}
+//function
+function fullName(first, second) {
+    const name = first + '' + second;
+}
+fullName('rahim', 'ali');
+
+const bottle = { color: 'yellow', contain: 'water' };
