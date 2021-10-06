e6926f844f6f51c17c87dd0f5c1ae9adb8b2fe8c
dipdastoko <dipdastoko@gmail.com>
2021-10-06 18:40:36 +0600

    scope

---
+++ js-concepts/scope.js
@@ -0,0 +1,13 @@
+const favNum = 27;
+
+function add(first, second) {
+    const result = first + second;
+    if (result > 9) {
+        var mood = 'happy';
+        mood = 'crancky';
+    }
+    console.log(mood);
+}
+
+const sum = add(11, 35);
+console.log(mood);
