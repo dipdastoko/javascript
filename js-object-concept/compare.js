43879c91e0439df30c6f1ae985104dc6f58d744e
dipdastoko <dipdastoko@gmail.com>
2021-10-07 17:30:22 +0600

    loop Object

---
+++ js-object-concept/compare.js
@@ -0,0 +1,26 @@
+const bottle = {
+    color: 'yellow',
+    hold: 'water',
+    price: 50,
+    isCleaned: true
+};
+/*
+for(let i=0; i<10; i++){}
+for(const num of numbers){} //array
+for(const prop in student){} //object
+ */
+
+for (const prop in bottle) {
+    // console.log(prop, bottle[prop]);
+}
+
+const keys = Object.keys(bottle);
+for (const prop of keys) {
+
+    // console.log(prop, bottle[prop]);
+}
+
+//advanced
+for (const [key, value] of Object.entries(bottle)) {
+    console.log(key, value);
+}
