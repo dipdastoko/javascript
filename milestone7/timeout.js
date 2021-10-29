2ae70c6af5cab33e620f84c46c03ddf516c71ded
dipdastoko <dipdastoko@gmail.com>
2021-10-29 16:45:50 +0600

    setTimeout

---
+++ milestone7/timeout.js
@@ -0,0 +1,10 @@
+function doSomething() {
+    console.log('I am coding javascript');
+}
+console.log('First: ami sobar aage');
+console.log('Second: ami second person');
+setTimeout(function () {
+    console.log('I am using VS code');
+}, 5000);
+console.log('Third: ami 3 number bachha');
+console.log('Fourth: ami 4 number bachha');
